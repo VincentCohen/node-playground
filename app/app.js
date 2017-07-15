4af1aa906424cd824bd5530fbb8f2beef64d634e
Vincent <vincent@getseoshop.com>
2017-07-15 14:55:11 +0200

    Added mongo

--- app/app.js
+++ app/app.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('NodeJs playground!\n');
+  res.send('NodeJs playground!');
 });
 
 app.get('/invoices', (req, res) => {
