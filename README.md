aebad21a6690a57c43799c298d405960be8642b3
Vincent <vincent@getseoshop.com>
2017-07-15 12:58:20 +0200

    Setting up NodeJs & Nodemon

---
+++ README.md
@@ -0,0 +1,9 @@
+# Node Playground
+
+Trying out things that do stuff to test something
+
+This container is based on https://github.com/b00giZm/docker-compose-nodejs-examples/
+
+## Run docker container
+- `docker-compose build` 
+- `docker-compose up`
---
+++ app/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const express = require('express');
+
+// Constants
+const PORT = 3000;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+  res.send('NodeJs playground!\n');
+});
+
+app.get('/invoices', (req, res) => {
+  res.send('invoices\n');
+});
+
+app.get('/invoices/{id}', (req, res) => {
+  res.send('single invoice\n');
+});
+
+app.get('/invoices/{id}/items', (req, res) => {
+  res.send('invoice items listing\n');
+});
+
+app.get('/invoices/{id}/print', (req, res) => {
+  res.send('print a single invoice\n');
+});
+
+app.listen(PORT, HOST);
+
+console.log(`Running on http://${HOST}:${PORT}`);
